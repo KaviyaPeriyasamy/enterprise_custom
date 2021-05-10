8aedeb52eca27ea0e215df5ac841bef72559e764
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-05-10 20:41:23 +0530

    si table updated

--- enterprise_custom/customize.js
+++ enterprise_custom/customize.js
@@ -1,5 +1,5 @@
-frappe.ui.form.on("Sales Invoice Item", "square_feet", function(doc, cdt, cdn) {
+frappe.ui.form.on("Sales Invoice Item", "sqft", function(doc, cdt, cdn) {
 	var d = locals[cdt][cdn];
-    d.amount = d.rate * d.square_feet * d.qty
+    d.amount = d.rate * d.sqft * d.qty
 	refresh_field('items');
 });
