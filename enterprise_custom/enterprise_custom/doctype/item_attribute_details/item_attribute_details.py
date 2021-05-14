f0febd54ce77f129f9c6338c926a8c9591c23713
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-05-14 22:22:08 +0530

    feat: update get attribute function

--- enterprise_custom/enterprise_custom/doctype/item_attribute_details/item_attribute_details.py
+++ enterprise_custom/enterprise_custom/doctype/item_attribute_details/item_attribute_details.py
@@ -3,8 +3,12 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ItemAttributeDetails(Document):
 	pass
+
+@frappe.whitelist()
+def get_item_attribute(item):
+	return frappe.db.get_value('Item Attribute Details', {'parent':item, 'enabled':1}, 'attribute')
